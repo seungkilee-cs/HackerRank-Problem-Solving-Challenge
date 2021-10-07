c95fd87975e2ab49e1f757fedd817e940587ba69
Seung Ki Lee <seungkilee.cs@gmail.com>
2021-10-07 17:55:42 -0500

    init

---
+++ ProblemSolving/03.diagonalDifference.py
@@ -0,0 +1,116 @@
+'''
+2021/10/06 W
+
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+int arr[n][m]: an array of integers
+Return
+
+int: the absolute diagonal difference
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the square matrix .
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+'''
+
+'''
+for i to n
+ltr = [i][i]
+rtl = [i][n-i]
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    # -1 on the length for indexihg
+    res, ltr, rtl, rowLen = 0, 0, 0, len(arr)-1
+    
+    # get ltr & rtl diagonal sum by looping top layer
+    # rowLen + 1 so it loops through the last row
+    for i in range(0, rowLen + 1):
+        ltr += arr[i][i]
+        rtl += arr[i][rowLen - i]
+    
+    print(ltr)
+    print(rtl)
+    
+    res = ltr - rtl
+    if res < 0:
+        return -1 * res
+    else:
+        return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
